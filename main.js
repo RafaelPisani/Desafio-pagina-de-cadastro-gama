37caef222cfe8736ccd0b0a0c599b36706a83dcf
Rafael Pisani <59885763+RafaelPisani@users.noreply.github.com>
2021-07-25 12:58:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+main
+
+
+function controleformproduto() {
+    var form = document.getElementById("formproduct")
+
+    form.style.display = "block";
+}
+
+function cadastrarProduto() {
+    var produtoDescrição = document.getElementById("produtoDescrição");
+    var produtoQuantidade = document.getElementById("produtoDescrição");
+
+    var dados = Json.parse(localstorage.getItem("dadosProduto"));
+
+    if (dados == null) {
+        localStorage.setItem("dadosproduto", "[]")
+        dados = [];
+    }
+    var auxRegistro = {
+        nome: produtoDescrição.value,
+        quantidade: produtoQuantidade.value
+    }
+    dados.push(auxRegistro);
+
+    dados.setItem("dadosProduto", "[]");
+
+    varpush(auxRegistro);
+
+    localstorage.setItem("dadosProduto", Json.stringify(dados))
+    alert("Registro incluído com sucesso!")
+
+    produtoDescrição.value = "";
+    produtoQuantidade.value = "";
+
+}
